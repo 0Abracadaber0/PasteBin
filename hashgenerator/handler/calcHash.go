65766cf5903119e5e7d6c04ba884a423d429d98e
Yauheni <yauhenbelarus@gmail.com>
2024-06-12 10:11:55 +0300

    pastebin can send request to hashgenerator

--- hashgenerator/handler/calcHash.go
+++ hashgenerator/handler/calcHash.go
@@ -4,6 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 func CalcHashHandler(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
-		"hash": "HIIIII",
+		"hash": "This is hash",
 	})
 }
--- pastebin/handler/paste.go
+++ pastebin/handler/paste.go
@@ -1,7 +1,33 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
 func PasteHandler(c *fiber.Ctx) error {
-	return c.SendFile("templates/paste.html")
+	url := "http://localhost:8081/api/calc_hash"
+
+	a := fiber.AcquireAgent()
+	req := a.Request()
+	req.Header.SetMethod("POST")
+	req.SetRequestURI(url)
+
+	if err := a.Parse(); err != nil {
+		panic(err)
+	}
+
+	_, body, errs := a.Bytes()
+	if errs != nil {
+		log.Fatal(errs)
+	}
+
+	var data fiber.Map
+	err := json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c.Render("templates/paste.html", data)
 }
