8013683a7ee16bea26e90b5810743cb1293c3114
Yauheni <yauhenbelarus@gmail.com>
2024-07-15 11:36:53 +0300

    added initialize of logger

--- pastebin/cmd/main.go
+++ pastebin/cmd/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"log"
+	"main/config"
 	"main/database"
 	"main/router"
 )
 
 func main() {
+	log := config.SetupLogger(config.Config("ENV"))
+	log.Info("starting server", "env", config.Config("ENV"))
+
 	app := fiber.New()
 
 	database.ConnectDB()
 
 	router.SetUpRoutes(app)
 
-	log.Fatal(app.Listen("0.0.0.0:8080"))
+	err := app.Listen("0.0.0.0:8080")
+	if err != nil {
+		return
+	}
 }
